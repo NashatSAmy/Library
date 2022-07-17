83dd83ded284231b71462f4369253caacd8a415a
Nashat Samy <nashat.msamy@gmail.com>
2022-07-17 11:19:22 +0200

    just finished the library site.

--- main.js
+++ main.js
@@ -18,13 +18,19 @@ let libraryIndex = 0;
 
 function addBookToLibrary(e) {
   const author = document.getElementById("author");
-  const title = document.getElementById("title");  
-  const pages = document.getElementById("pageNo"); 
+  const title = document.getElementById("title");
+  const pages = document.getElementById("pageNo");
   const read = document.getElementById("read");
-  
-  if(e.target.id == "submit") { 
-    const book = new Book(author.value, title.value, pages.value, read.checked === true ? "Yes" : "No");
+
+  if (e.target.id == "submit") {
+    const book = new Book(
+      author.value,
+      title.value,
+      pages.value,
+      read.checked === true ? "Yes" : "No"
+    );
     myLibrary.push(book);
+    displayBook();
   }
   return;
 }
@@ -35,13 +41,15 @@ function displayBook() {
     book.classList.add("shelf");
     book.innerHTML = `
     <button class="fa fa-remove" id="remove"></button>
-    <span class="author">Author ${myLibrary[i].author}: </span>
+    <span class="author">Author: ${myLibrary[i].author} </span>
     <span class="title">Title: ${myLibrary[i].title}</span>
     <span class="pageNo">Number of pages: ${myLibrary[i].pages}</span>
     <span class="read"
-      >Read: ${myLibrary[i].read}
+      ><span>Read: ${myLibrary[i].read}</span>
       <label class="switch">
-        <input type="checkbox" />
+        <input type="checkbox" id="readStatus" ${
+          myLibrary[i].read == "Yes" ? "checked" : false
+        }/>
         <span class="slider"></span> </label
     ></span>`;
     booksShelf[0].appendChild(book);
@@ -53,7 +61,7 @@ function showBookForm() {
   let popup = document.createElement("div");
   popup.classList.add("popup");
   popup.setAttribute("id", "popup");
-  popup.innerHTML = `<form action="#" onSubmit="return false;">
+  popup.innerHTML = `<form action="#">
   <button class="fa fa-remove" id="removeForm" name="formRemove"></button>
   <label for="author"
     >Author: <input type="text" name="author" id="author" required autocomplete="off"
@@ -67,19 +75,35 @@ function showBookForm() {
   /></label>
   <label for="read">Read it? </label
   ><input type="checkbox" name="read" id="read" />
-  <button type="submit" id="submit" onClick="(e)=>e.preventdefault()">Add Book</button>
+  <button type="button" id="submit">Add Book</button>
 </form>`;
 
-document.body.prepend(popup);
+  document.body.prepend(popup);
 }
 
-function closeFrom(e) {
+function closeFrom(e, remove = false) {
   const popup = document.getElementById("popup");
-  if (e.target.id == "removeForm" || e.target.id == "popup" || e.target.id == "submit") {
+  if (e.target.id == "removeForm" || e.target.id == "popup" || remove == true) {
     popup.remove();
   }
 }
 
+function removeBook(e) {
+  if (e.target.id !== "remove") return;
+  e.target.parentElement.remove();
+}
+
+function changeReadStatus(e) {
+  if (e.target.id !== "readStatus") return;
+  console.log(e.target.parentElement.parentElement.innerText);
+  e.target.parentElement.parentElement.firstChild.innerText =
+    e.target.parentElement.parentElement.firstChild.innerText == "Read: Yes"
+      ? "Read: No"
+      : "Read: Yes";
+}
+
 addBookButton.addEventListener("click", showBookForm);
 window.addEventListener("click", closeFrom);
-window.addEventListener("click", addBookToLibrary)
+window.addEventListener("click", addBookToLibrary);
+window.addEventListener("click", removeBook);
+window.addEventListener("click", changeReadStatus);
